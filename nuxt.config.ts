1b259bd20d40451ec443de2b622a31b60a1d7e32
Christian <christian.palladini@linksmt.it>
2024-01-04 14:12:36 +0100

    chore: add basic modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@vueuse/nuxt",
+    "@unocss/nuxt",
+    "@nuxt/content",
+    "@nuxt/color-mode",
+    "@nuxtjs/color-mode"
+  ]
 })
---
+++ uno.config.ts
@@ -0,0 +1,7 @@
+ import { defineConfig, presetUno, presetIcons, presetAttributify } from 'unocss'
+
+export default defineConfig({
+    presets: [
+        presetUno(), presetIcons(), presetAttributify()
+    ]
+})
